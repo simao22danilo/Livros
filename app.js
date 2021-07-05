75ae4178a0c5b9c64a20ec446fb8c50fd2d432a7
Danilo <simao22danilo@gmail.com>
2021-07-05 11:01:06 -0400

    Criando Projeto Livros

---
+++ app.js
@@ -0,0 +1,32 @@
+const livros = require("./database");
+
+//pegar o input
+//Se for sim, mostra as categorias disponíveis, pergunta qual categoria ela escolhe
+//Se não, mostra todos os livros em ordem crescente pela quantidade de páginas
+
+const readline = require("readline-sync");
+
+const entradaInicial = readline.question("Deseja buscar um livro?S/N");
+
+if (entradaInicial.toLocaleUpperCase() === "S") {
+  console.log("Essas são as categorias disponíveis:");
+  console.log(
+    "Produtividade",
+    "/História brasileira",
+    "/Américas",
+    "/Estilo de vida",
+    "/Tecnologia"
+  );
+
+  const entradaCategoria = readline.question("Qual categoria você escolhe:");
+
+  const retorno = livros.filter(
+    (livro) => livro.categoria === entradaCategoria
+  );
+
+  console.table(retorno);
+} else {
+  const livrosOrdenados = livros.sort((a, b) => a.paginas - b.paginas);
+  console.log("Esses são todos os livros disponíveis:");
+  console.table(livrosOrdenados);
+}
---
+++ database.js
@@ -0,0 +1,58 @@
+const livros = [
+  {
+    id: 1,
+    nome: "Digital Minimalisn",
+    autor: "Cal Newport",
+    categoria: "Produtividade",
+    paginas: 254,
+    recomenda: false,
+    leu: false,
+  },
+  {
+    id: 2,
+    nome: "O Genocidio do negro brasileiro",
+    autor: "Abdias do Nascimento",
+    categoria: "História brasileira",
+    paginas: 254,
+    recomenda: false,
+    leu: false,
+  },
+  {
+    id: 3,
+    nome: "As veias abertas da américa latina",
+    autor: "Eduardo galeano",
+    categoria: "Américas",
+    paginas: 400,
+    recomenda: false,
+    leu: false,
+  },
+  {
+    id: 4,
+    nome: "Algoritmos para viver",
+    autor: "Brian Cheistian",
+    categoria: "Tecnologia",
+    paginas: 120,
+    recomenda: true,
+    leu: true,
+  },
+  {
+    id: 5,
+    nome: "Thinking, first and slow",
+    autor: "Daniel Kahneman",
+    categoria: "Estilo de vida",
+    paginas: 418,
+    recomenda: true,
+    leu: true,
+  },
+  {
+    id: 6,
+    nome: "Padrões Javascript",
+    autor: "Stoyan Stefanov",
+    categoria: "Tecnologia",
+    paginas: 231,
+    recomenda: true,
+    leu: true,
+  },
+];
+
+module.exports = livros;
